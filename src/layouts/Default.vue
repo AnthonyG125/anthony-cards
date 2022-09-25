3bf872db33b35669c3b93fcaca742295c37c507c
Anthony <anthony.gras@hotmail.com>
2022-09-26 09:37:09 +1100

    Initial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="layout">
+    <slot/>
+  </div>
+</template>
+
+
+
+<style>
+body {
+  font-size: 15px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background: #222;
+  color: #fff;
+  font-family: Nunito,Helvetica,Arial,sans-serif;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.layout {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,55 @@
+<template>
+  <Layout>
+
+    <div class="content">
+      <h1 class="name">Anthony Gras</h1>
+
+      <svg width="200px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><g id="Hacker_anonymous"><path d="M475.3571,413.24a69.9,69.9,0,0,0-39.8845-57.4407l-39.9287-18.7987,21.5791-44.5621a89.4527,89.4527,0,0,0,.0025-77.9684L359.7988,96.0682C317.7933,9.3105,194.2088,9.31,152.2019,96.0666L94.87,214.4745a89.445,89.445,0,0,0,.0049,77.9692l21.581,44.5569L76.5256,355.8a69.898,69.898,0,0,0-39.8831,57.439l-3.612,43.3773a22.5157,22.5157,0,0,0,22.4381,24.3842H456.5337A22.5134,22.5134,0,0,0,478.97,456.6187ZM364,260.1205a107.9746,107.9746,0,0,1-98.1035,107.5V341.1249a9.8965,9.8965,0,0,0-19.793,0v26.4957A107.9746,107.9746,0,0,1,148,260.1205V203.44a28.8192,28.8192,0,0,1,28.8193-28.8193H335.1806A28.8193,28.8193,0,0,1,364,203.44Z"/><path d="M321.8213,275.9979a9.91,9.91,0,0,0-12.3135,6.6709,13.5776,13.5776,0,0,1-26.0156,0,9.9026,9.9026,0,1,0-18.9844,5.6426,33.3877,33.3877,0,0,0,63.9844,0A9.9125,9.9125,0,0,0,321.8213,275.9979Z"/><path d="M240.8213,275.9979a9.8908,9.8908,0,0,0-12.3135,6.6709,13.5776,13.5776,0,0,1-26.0156,0,9.9026,9.9026,0,1,0-18.9844,5.6426,33.3877,33.3877,0,0,0,63.9844,0A9.9125,9.9125,0,0,0,240.8213,275.9979Z"/><path d="M319,227.4384H283a9.8965,9.8965,0,1,0,0,19.7929h36a9.8965,9.8965,0,1,0,0-19.7929Z"/><path d="M193,247.2313h36a9.8965,9.8965,0,1,0,0-19.7929H193a9.8965,9.8965,0,1,0,0,19.7929Z"/></g></svg>
+      <h3 class="info">Developer at Caledonienne de Solution Business (CSB) & student at IUT</h3>
+
+      <div class="lien">
+        <a aria-label="Contact me"
+           data-microtip-position="top" role="tooltip" href="mailto:anthony.gras@hotmail.com"
+           title="Contact me">
+          <svg role="img" width="2rem" height="2rem" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Contact Us</title>
+            <defs>
+              <linearGradient id="linear-gradient" x1="0.5" x2="0.5" y2="1" gradientUnits="objectBoundingBox">
+                <stop offset="0" stop-color="#E6CF1C"></stop><stop offset="1" stop-color="#109F10"></stop>
+              </linearGradient>
+            </defs>
+            <path d="M12 12.713l-11.985-9.713h23.971l-11.986 9.713zm-5.425-1.822l-6.575-5.329v12.501l6.575-7.172zm10.85 0l6.575 7.172v-12.501l-6.575 5.329zm-1.557 1.261l-3.868 3.135-3.868-3.135-8.11 8.848h23.956l-8.11-8.848z" fill="url(#linear-gradient)">
+            </path>
+          </svg>
+        </a>
+      </div>
+    </div>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Anthony GRAS - Visit Card'
+  }
+}
+</script>
+
+<style>
+.name {
+  font-size: 2.5rem;
+  font-family: Droid,Times,serif;
+}
+
+
+
+.info {
+  color: #c7c7c7;
+  font-size: 1.25rem;
+  font-family: Nunito,Helvetica,Arial,sans-serif;
+}
+
+.content{
+  text-align: center;
+}
+</style>
